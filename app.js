708213692f87df3d607680b6e8a3fff9b23c45eb
nobuko.m <animopara100pre@gmail.com>
2020-08-27 10:23:16 +0900

    [Finish]03

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
-// 足し算
-alert(1 + 10);
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+    while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+      alert('グー・チョキ・パーのいずれかを入力して下さい');
+      user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+    }
+    var js_hand = getJShand();
+    var judge = winLose(user_hand, js_hand);
 
-// 引き算
-alert(10 - 1);
+    if (user_hand != null) {
+        alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+    } else {
+        alert("またチャレンジしてね")
+    }
 
-// 掛け算
-alert(10 * 1);
+    function getJShand(){
+        var js_hand_num = Math.floor( Math.random() * 3 );
+        var hand;
+        if(js_hand_num == 0){
+            hand = "グー";
+        }else if(js_hand_num == 1){
+            hand = "チョキ";
+        }else if(js_hand_num == 2){
+            hand = "パー";
+        }
+        return hand;
+    }
 
-// 割り算
-alert(10 / 2);
+    function winLose(user, js){
+        var winLoseStr;
+        if(user == "グー"){
+            if(js == "グー"){
+                winLoseStr = "あいこ";
+            }else if(js == "チョキ"){
+                winLoseStr = "勝ち";
+            }else if(js == "パー"){
+                winLoseStr = "負け";
+            }
+        }else if(user == "チョキ"){
+            if(js == "グー"){
+                winLoseStr = "負け";
+            }else if(js == "チョキ"){
+                winLoseStr = "あいこ";
+            }else if(js == "パー"){
+                winLoseStr = "勝ち";
+            }
+        }else if(user == "パー"){
+            if(js == "グー"){
+                winLoseStr = "勝ち";
+            }else if(js == "チョキ"){
+                winLoseStr = "負け";
+            }else if(js == "パー"){
+                winLoseStr = "あいこ";
+            }
+        }
+        return winLoseStr;
+    }
